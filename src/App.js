47b21909844ee05584b1294c57cae22ca21adb63
Yevhenii Bereziuk <yevhenii.bereziuk@namecheap.com>
2024-06-12 06:02:59 +0300

    change app text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello from Bereziuk study way!
+          Hello from Bereziuk study way! I was changed on the feature branch.
         </p>
         <a
           className="App-link"
